35f8bed7c44011bb403b2912794d081a059d9ab0
Johan Vester <johan.vester@gmail.com>
2019-03-28 08:54:06 +0100

    Add useMap(), showing how to decode JSON into map[string]interface{}

--- main.go
+++ main.go
@@ -11,25 +11,59 @@ type Person struct {
 }
 
 func main() {
-	// Read entire file contents to memory
+	useMap()
+}
+
+func useStruct() {
+	// Read entire file contents to memory.
 	data, _ := ioutil.ReadFile("data.json")
 
-	// Allocate Person
+	// Allocate Person.
 	p := Person{}
 
-	// Decode json into Person
+	// Decode json into Person.
 	err := json.Unmarshal(data, &p)
 	if err != nil {
 		panic(err)
 	}
 
-	// Update age
-	p.Age = 42
+	// Update age.
+	p.Age = p.Age + 5
 
-	// Decode Person into json
+	// Encode Person into json.
 	out, _ := json.Marshal(p)
 
-	// Write new json to file
+	// Write new json to file.
+	err = ioutil.WriteFile("new-data.json", out, 0644)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func useMap() {
+	// Read entire file contents to memory.
+	data, _ := ioutil.ReadFile("data.json")
+
+	// Create map.
+	var p map[string]interface{}
+
+	// Decode json.
+	err := json.Unmarshal(data, &p)
+
+	// Read age, and cast it from interface{} to float64.
+	//
+	// All json numbers have to be cast to float64 initially, as there are no
+	// separate types for integers and floating point values in json.
+	// Further conversion must be done with another step.
+	age := p["age"].(float64)
+
+	// Update age
+	p["age"] = age + 5.0
+
+	// Encode map into json
+	out, _ := json.Marshal(p)
+
+	// Write new json to file.
 	err = ioutil.WriteFile("new-data.json", out, 0644)
 	if err != nil {
 		panic(err)
